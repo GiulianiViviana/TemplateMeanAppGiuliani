523ec250784057d36184123a2969c489e0ee3cef
GiulianiViviana <giuliani.viviana@iisgalvanimi.edu.it>
2019-10-15 10:53:08 +0000

    Finire domande

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,15 +8,19 @@ import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
 })
 export class AppComponent implements OnInit{
   results: string[];
+  o : Observable<Object>;
   private BASE_URL:string = '/api';
     // Inject HttpClient into your component or service.
     constructor(private http: HttpClient) {}
 
     ngOnInit(): void {
       // Make the HTTP request:
-      this.http.get('https://3000-e8ad746d-1e02-48e9-9a6d-e4103ff17893.ws-eu0.gitpod.io/api').subscribe(data => {
-        // Read the result field from the JSON response.
-        this.results = data['results'];
-      });
+      this.o = this.http.get('https://3000-e8ad746d-1e02-48e9-9a6d-e4103ff17893.ws-eu0.gitpod.io/api');
+      this.o.subscribe(this.getData);
     }
+    getData = (data : Object) =>
+   {
+     this.results = data['results'];
+   }
+
 }
